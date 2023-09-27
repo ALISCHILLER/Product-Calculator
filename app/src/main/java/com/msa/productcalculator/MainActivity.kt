e8f5cd9b58ff97d1e3259670f838f0444e3221fa
SOFTWARE-SV\zar <zartablet2022@gmail.com>
2023-09-27 18:14:32 +0330

    c

--- app/src/main/java/com/msa/productcalculator/MainActivity.kt
+++ app/src/main/java/com/msa/productcalculator/MainActivity.kt
@@ -3,12 +3,21 @@ package com.msa.productcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.msa.productcalculator.ui.theme.ProductCalculatorTheme
 
@@ -20,9 +29,26 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
-                    ProductCalculatorDialog()
+
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ){
+
+
+                        var hideDatePicker by remember {
+                            mutableStateOf(true)
+                        }
+                        Button(
+                            onClick = { hideDatePicker = false }
+                        ) {
+                            Text("کالا")
+                        }
+                        if (!hideDatePicker)
+                            ProductCalculatorDialog()
+                    }
                 }
             }
         }
